a4fd9f49fbb340e03f967f5333036f3880dc120d
xuzhou1865 <gistarbird@gmail.com>
2026-04-02 09:10:23 +0800

    test123

--- blog.config.js
+++ blog.config.js
@@ -65,7 +65,7 @@ const BLOG = {
   // 欢迎语打字效果,Hexo,Matery主题支持, 英文逗号隔开多个欢迎语。
   GREETING_WORDS:
     process.env.NEXT_PUBLIC_GREETING_WORDS ||
-    '一个痴迷于自由的构建者，欢迎你的到来~',
+    '欢迎你的到来~',
 
   // uuid重定向至 slug
   UUID_REDIRECT: process.env.UUID_REDIRECT || false
